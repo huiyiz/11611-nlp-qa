cef3810127d656347b6b9d761a8e7eda18a89cdf
guoyaol <guoyaoli00@gmail.com>
2023-04-18 03:04:24 +0000

    final QG

---
+++ test.sh
@@ -0,0 +1,7 @@
+SHARE=$(pwd)
+IMAGE=$1
+NAME=$(sudo docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
+echo '****************'
+sudo docker exec -i $NAME ./ask /host/Users/data/set1/a1.txt 20 2>/dev/null
+echo '****************'
+sudo docker stop $NAME >/dev/null
